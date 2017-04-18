c0b20a0787561d45faf6ce72a549672d5f974a41
lorenzo jokhan <lorenzo jokhan>
2017-04-18 10:44:00 +0200

    Mockup test class created to prove junit in jenkins works v2

--- src/test/java/MockTest.java
+++ src/test/java/MockTest.java
@@ -1,10 +1,12 @@
 import junit.framework.TestCase;
+import org.junit.Test;
 
 /**
  * Created by Lorenzo Jokhan on 18/04/2017.
  */
 public class MockTest extends TestCase {
 
+    @Test
     public void fakeTestTrue() {
         String text = "HelloWorld";
         assertEquals("HelloWorld", text);
